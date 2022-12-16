7a6b69acca67cb9c06c95f50c37f2af36cb95816
Gary_SONY <garynova781@gmail.com>
2022-12-16 20:49:26 +0800

    update the control of scope setting testing

--- scope_set_index.py
+++ scope_set_index.py
@@ -430,14 +430,65 @@ class scope_config():
 
             pass
 
+        elif setup_index == 'NT50970_pwr_seq_inrush':
+            # default set for inrush checking, C4 use Vin, change in pwr_sub program
+            '''
+            221205
+            to improve the find signal, add two dimension dictionary, and change the offset setting to
+            nornalization result, x * volt_div, from +3 to -3
+            use the 'v_offset_ind' as the new offset normalization index
+            '''
+
+            self.ch_c1 = {'ch_view': 'TRUE', 'volt_dev': '2', 'BW': '20MHz', 'filter': '2bits', 'v_offset': 2,
+                          'label_name': 'Buck', 'label_position': 0.001, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 1}
+            self.ch_c2 = {'ch_view': 'TRUE', 'volt_dev': '2', 'BW': '20MHz', 'filter': '2bits', 'v_offset': -2,
+                          'label_name': 'LDO', 'label_position': 0.001, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': -1}
+            self.ch_c3 = {'ch_view': 'TRUE', 'volt_dev': '2', 'BW': '20MHz', 'filter': '2bits', 'v_offset': -1,
+                          'label_name': 'EN2_pin', 'label_position': 0.001, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': -0.5}
+            self.ch_c4 = {'ch_view': 'TRUE', 'volt_dev': '2', 'BW': '20MHz', 'filter': '3bits', 'v_offset': -6,
+                          'label_name': 'Vin', 'label_position': 0.001, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': -1.5}
+            self.ch_c5 = {'ch_view': 'TRUE', 'volt_dev': '0.5', 'BW': '20MHz', 'filter': '2bits', 'v_offset': -0.6,
+                          'label_name': 'Iin', 'label_position': 0.001, 'label_view': 'TRUE', 'coupling': 'DC50', 'v_offset_ind': -3}
+            self.ch_c6 = {'ch_view': 'TRUE', 'volt_dev': '2', 'BW': '20MHz', 'filter': '2bits', 'v_offset': 0,
+                          'label_name': 'VCC', 'label_position': 0.001, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 0}
+            self.ch_c7 = {'ch_view': 'FALSE', 'volt_dev': '2', 'BW': '20MHz', 'filter': '2bits', 'v_offset': 0,
+                          'label_name': 'VOP', 'label_position': 0.001, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': -2}
+            self.ch_c8 = {'ch_view': 'TRUE', 'volt_dev': '2', 'BW': '20MHz', 'filter': '3bits', 'v_offset': -7,
+                          'label_name': 'EN1_pin', 'label_position': 0.001, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': -2}
+
+            # add the two dimension index for the find signal reference
+            self.ch_index = {'C1': self.ch_c1, 'C2': self.ch_c2, 'C3': self.ch_c3, 'C4': self.ch_c4,
+                             'C5': self.ch_c5, 'C6': self.ch_c6, 'C7': self.ch_c7, 'C8': self.ch_c8}
+
+            # setting of general
+            self.set_general = {'trigger_mode': 'Auto', 'trigger_source': 'C4', 'trigger_level': '1.8',
+                                'trigger_slope': 'Positive', 'time_scale': '0.0005',
+                                'time_offset': '-0.002', 'sample_mode': 'RealTime', 'fixed_sample_rate': '1.25GS/s'}
+
+            # setting of measurement
+            self.p1 = {"param": "pkpk", "source": "C1", "view": "TRUE"}
+            self.p2 = {"param": "pkpk", "source": "C2", "view": "TRUE"}
+            self.p3 = {"param": "pkpk", "source": "C6", "view": "TRUE"}
+            self.p4 = {"param": "max", "source": "C3", "view": "TRUE"}
+            self.p5 = {"param": "min", "source": "C7", "view": "TRUE"}
+            self.p6 = {"param": "max", "source": "C5", "view": "TRUE"}
+            self.p7 = {"param": "pkpk", "source": "C7", "view": "TRUE"}
+            self.p8 = {"param": "pkpk", "source": "C3", "view": "TRUE"}
+            self.p9 = {"param": "mean", "source": "C3", "view": "TRUE"}
+            self.p10 = {"param": "mean", "source": "C4", "view": "TRUE"}
+            self.p11 = {"param": "mean", "source": "C6", "view": "TRUE"}
+            self.p12 = {"param": "mean", "source": "C2", "view": "TRUE"}
+
+            pass
+
         else:
             # if index wrong, back to 374 settings
             self.ch_c1 = {'ch_view': 'TRUE', 'volt_dev': '0.02', 'BW': '20MHz', 'filter': '2bits', 'v_offset': -3.3,
-                          'label_name': 'AVDD', 'label_position': 0, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 1}
+                          'label_name': 'CH1', 'label_position': 0, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 1}
             self.ch_c2 = {'ch_view': 'TRUE', 'volt_dev': '0.02', 'BW': '20MHz', 'filter': '2bits', 'v_offset': 3.3,
-                          'label_name': 'OVSS', 'label_position': 0, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 1}
+                          'label_name': 'CH2', 'label_position': 0, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 1}
             self.ch_c3 = {'ch_view': 'TRUE', 'volt_dev': '0.2', 'BW': '20MHz', 'filter': '2bits', 'v_offset': 3.5,
-                          'label_name': 'VON', 'label_position': 0, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 1}
+                          'label_name': 'CH3', 'label_position': 0, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 1}
             self.ch_c4 = {'ch_view': 'TRUE', 'volt_dev': '1', 'BW': '20MHz', 'filter': '2bits', 'v_offset': -3,
                           'label_name': 'Vin', 'label_position': 0, 'label_view': 'TRUE', 'coupling': 'DC1M', 'v_offset_ind': 1}
             self.ch_c5 = {'ch_view': 'TRUE', 'volt_dev': '0.02', 'BW': '20MHz', 'filter': '2bits', 'v_offset': -0.06,
@@ -466,5 +517,27 @@ class scope_config():
             self.p10 = {"param": "mean", "source": "C4", "view": "TRUE"}
             self.p11 = {"param": "mean", "source": "C6", "view": "TRUE"}
             self.p12 = {"param": "mean", "source": "C2", "view": "TRUE"}
+
+        pass
+
+
+if __name__ == '__main__':
+    #  the testing code for this file object
+    import parameter_load_obj as par
+    excel_t = par.excel_parameter('obj_main')
+    sim_scope = 1
+    default_path = 'C:\\py_gary\\test_excel\\wave_form_raw\\'
+
+    import Scope_LE6100A as sco
+
+    # scope = Scope_LE6100A('GPIB: 5', 3, sim_scope, excel_t)
+    scope = sco.Scope_LE6100A(excel0=excel_t)
+
+    test_index = 0
+
+    if test_index == 0:
+
+        scope.open_inst()
+        scope.scope_initial('NT50970_pwr_seq_inrush')
 
         pass
