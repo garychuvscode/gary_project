b64e52e3be81c4b79465eaa93e6c73af6253bc25
Gary_surf7 <garynova781@gmail.com>
2023-10-08 23:57:07 +0800

    update for G_RPC now is able to operate full efficiency run for Buck 6 packed as a group, collect all result in one sheet and no extra window left after program finished

--- G_RPC.py
+++ G_RPC.py
@@ -1,10 +1,31 @@
 from multiprocessing.connection import Client, wait
 
+# for the excel sheet control
+# only the main program use this method to separate the excel and program
+# since here will include the loop structure and overall control of report generation
+import sheet_ctrl_main_obj as sh
 
 # excel parameter and settings
-import parameter_load_obj as par
+import parameter_load_obj as para
 import time
 import xlwings as xw
+import pyautogui
+# fmt: off
+
+import inst_pkg_d as inst
+
+# define the excel object
+excel_m = para.excel_parameter(str(sh.file_setting))
+
+pwr_m = inst.LPS_505N(
+    excel_m.pwr_vset,
+    excel_m.pwr_iset,
+    excel_m.pwr_act_ch,
+    excel_m.pwr_supply_addr,
+    excel_m.pwr_ini_state,
+)
+
+pwr_m.sim_inst = 0
 
 # === other support object
 # add the report_arragement for building the result in the report sheet
@@ -14,7 +35,7 @@ import JIGM3 as mcu_g
 
 # === initialization for the support object
 file_name = "c:\\py_gary\\test_excel\\GPL_V5_RPC_temp.xlsx"
-rep_a = rep_obj.report_arragement(file_name0=file_name, full_trace0=1)
+rep_a = rep_obj.report_arragement(excel0=excel_m)
 
 mcu_m = mcu_g.JIGM3(sim_mcu0=0)
 print("JIGM3 MCU selected for Grace")
@@ -65,7 +86,7 @@ class NAGuiRPC:
             # mcu is also config as simulation mode
             mcu0 = mcu.MCU_control(0, 3)
             # using the main control book as default
-            excel0 = par.excel_parameter("obj_main")
+            excel0 = para.excel_parameter("obj_main")
             src0 = inst.Keth_2440(0, 0, 24, "off", "CURR", 15)
             src0.sim_inst = 0
             met_i0 = inst.Met_34460(0.0001, 7, 0.000001, 2.5, 20)
@@ -92,8 +113,18 @@ class NAGuiRPC:
         # LPS505
         self.pwr_ch = 3
 
-        # part_num for change sheet name
+        # part_num for change sheet name, version and extra comments
+        # plane to add in below format => this move to report_arragement object
         self.part_num = 'NT50374'
+        self.ext_comments = 'ext'
+
+        # buffer between report manager and GPL_V5
+        self.temp_res_bufffer = 'c:\\py_gary\\test_excel\\GPL_V5_RPC_temp.xlsx'
+        # window and other reference buffer:
+        self.grace_trace = 'c:\\py_gary\\test_excel\\grace_trace.xlsx'
+
+        # for closing all the workbooks in the app
+        self.GPL_app = 0
 
         pass
 
@@ -141,13 +172,15 @@ Efficiency.Run()
 
         return result
 
-    def buck_regulation_mix(self, mode0=0, setting_sel0=''):
+    def buck_regulation_mix(self, mode0=0, setting_sel0='', L_H0=1):
 
         '''
-        mode 0 => all, 1-LDO(L), 2-LDO_BK_on(LBO), 3-Buck_normal(BN), 4-Buck_usm(BU)
+        mode 0 => all, 1-LDO(L), 2-LDO_BK_on(LBO), 3-Buck_normal(BN), 4-Buck_usm(BU), 5-Buck_normal_H, 6-Buck_usm_H
         setting_sel0 => choose related settings:
         need to have below setting files before operation
         'setting_sel' + '_L'
+
+        L_H0 = 1 => add the operation of efficiency H part (high current)
 
         this item pack all the HV buck eff and regulation testing together
         1. LDO only mode (EN2 tight high through MCU if there are EN2)
@@ -165,8 +198,8 @@ Efficiency.Run()
         v_usm = 1.5
         v_off = 0
         i_o_curr = 0.2
-        mode_index = [ '_L', '_LBO', '_BN', '_BU' ]
-        vio_index = [ v_off, v_nor, v_nor, v_usm ]
+        mode_index = [ '_L', '_LBO', '_BN', '_BU', '_BN_H', '_BU_H' ]
+        vio_index = [ v_off, v_nor, v_nor, v_usm, v_nor, v_usm ]
 
         # device initialization for this item
         self.pwr_ini.chg_out(v_off, i_o_curr, self.pwr_ch, 'off')
@@ -195,9 +228,13 @@ Efficiency.Run()
             pass
 
         else:
-            # all run
+            # all run in mode high and low
 
-            c_mode = 4
+            if L_H0 == 0 :
+                c_mode = 4
+            else:
+                c_mode = 6
+
             x_mode = 0
 
             while x_mode < c_mode :
@@ -206,6 +243,7 @@ Efficiency.Run()
                 tag_set = setting_sel0 + mode_index[x_mode]
                 # finished EN1=L for LDO regulation
 
+                # assign the EN voltage for different operation
                 self.pwr_ini.chg_out(
                     vio_index[x_mode],
                     i_o_curr,
@@ -213,11 +251,17 @@ Efficiency.Run()
                     "on",
                 )
 
+                if x_mode == 0 :
+                    self.excel_ini.message_box('now is for "low" current mode, configure Iin and Iout to correct place', 'measurement current setting', auto_exception=1)
+                elif L_H0 == 1 and x_mode == 4:
+                    self.excel_ini.message_box('now is for "high" current mode, configure Iin and Iout to correct place', 'measurement current setting', auto_exception=1)
+
                 self.eff_run(tag_name0=tag_set)
 
                 # find the sheet and copy to the result temp
                 sh_anme = f'<{tag_set}> #1'
                 self.find_sh_all_books(sheet_name=sh_anme)
+                self.close_window_of_raw(x_mode)
 
                 print(f'all in one, the testing of {tag_set} is done')
                 x_mode = x_mode + 1
@@ -231,6 +275,7 @@ Efficiency.Run()
         pass
 
     def find_sh_all_books(self, sheet_name=''):
+
         # 连接到 Excel 应用程序
         # app = xw.App(visible=False)  # 如果不需要可见 Excel，请设置 visible=False
         app2 = xw.apps
@@ -240,24 +285,59 @@ Efficiency.Run()
         print(workbooks)
         print(app2)
         for app in app2:
+            print(f'now is app{app}')
             workbooks = app.books
             # 遍历所有已打开的工作簿
             for workbook in workbooks:
+                print(f'now is wb{workbook}')
                 # 遍历当前工作簿中的所有工作表
                 for sheet in workbook.sheets:
+                    print(f'now is wb{sheet}')
                     if sheet.name == sheet_name:
                         # 找到匹配的工作表
-                        print(f"在工作簿 '{workbook.name}' 中找到匹配的工作表：{sheet_name}")
+                        print(f"在app{app}工作簿 '{workbook.name}' 中找到匹配的工作表：{sheet_name}")
                         # 在这里可以执行其他操作，如读取或修改工作表内容
                         '''
                         copy this sheet to the result book, it should be able to find the
                         result book and reference sheet for copying index during the program operation
                         '''
-                        print(app.name)
                         # try to find the sheet, get the full trace,
                         # open books in same app
                         # and you can copy the sheet without pain
                         #
+
+                        # here is old version before 231008
+
+                        # record the app for app close
+                        self.GPL_app = app
+                        # open new books in the GPL_V5 app
+                        temp_wb = app.books.open(self.temp_res_bufffer)
+                        sh_ref= temp_wb.sheets('ref_sh')
+                        # sh_ref = temp_wb.sheets(1)
+                        # sh_ref.name = 'ref_sh'
+                        sheet.copy(sh_ref)
+                        temp_wb.save()
+                        temp_wb.close()
+
+                        # don't close workbook or you will turn off the connnection, just copy the sheet to GPL_temp
+
+                        # # first to get the full trace
+                        # result_trace = workbook.fullname
+                        # temp_sh_name = sheet.name
+                        # # add new book to prevent disconnect
+                        # app.activate
+                        # wb_space = xw.Book()
+                        # finded_wb = workbook
+                        # finded_wb.close()
+                        # # open in the original app
+                        # temp_wb = excel_m.app_org.books.open(result_trace)
+                        # print(f'books in apporg now {excel_m.app_org.books}')
+                        # # assign sheet in original app to temp_sh
+                        # temp_sh = temp_wb.sheets(temp_sh_name)
+                        # # copy the result to ref_sh in the result book
+                        # temp_sh.copy(rep_a.ref_sh)
+                        # print(f'copy the sheet from book "{workbook}" in app "{app}" to book "{rep_a.wb}" in app "{excel_m.app_org}"\n done')
+
 
 
                         break
@@ -267,37 +347,93 @@ Efficiency.Run()
 
         # 关闭 Excel 应用程序
         # app.quit()
+        self.close_raw_wb()
+        pass
 
         # return  new_sh
+    def close_raw_wb(self, save0=False, app0=0):
+        # deciede to turn off all the wb in app with or without saving
+        if app0 == 0 :
+            # use original app
+            app = self.GPL_app
+            pass
+        else:
+            app= app0
+
+        try:
+            # # 连接到指定的 Excel 应用程序实例
+            # app = self.GPL_app
+
+            # 获取应用程序实例中的所有打开的 Workbook
+            workbooks = app.books
+
+            # 遍历所有 Workbook 并关闭它们，不保存更改
+            for wb in workbooks:
+                wb.close()
+            app.quit()
+
+            print(f"已关闭 Excel 应用程序 '{app._pid}' 中的所有 Workbook,并且不保存更改。")
+
+        except Exception as e:
+            print(f"发生错误：{str(e)}")
+
+
+        pass
+
+    def close_window_of_raw(self, index0=0):
+        '''
+        turn off the related file window after finished progrm
+        '''
+        wb = self.excel_ini.app_org.books.open(self.grace_trace)
+        file_ref_sheet = wb.sheets('file_trace_ref')
+
+        default_row = 3
+        window_name = file_ref_sheet.range((default_row + index0, 7)).value
+        finded = 0
+
+        try:
+            # 获取当前打开的所有窗口标题
+            window_titles = pyautogui.getAllTitles()
+
+            # 遍历窗口标题，查找匹配的窗口
+            for title in window_titles:
+                if window_name in title:
+                    # 激活匹配的窗口
+                    pyautogui.getWindowsWithTitle(title)[0].activate()
+                    time.sleep(1)  # 等待窗口切换
+
+                    # 关闭当前激活的窗口
+                    pyautogui.hotkey("alt", "f4")
+
+                    print(f"已关闭窗口 '{window_name}'。")
+                    finded = 1
+                    pass
+
+
+
+        except Exception as e:
+            print(f"发生错误：{str(e)}")
+            pass
+
+        if finded == 0 :
+            print(f"未找到窗口标题为 '{window_name}' 的窗口。")
+        print(f'turn off this workbook: {wb}')
+        wb.close()
+        print(f'turn off FINISHED')
+
+        pass
+
 
 
 
 
 if __name__ == "__main__":
-    import inst_pkg_d as inst
 
-    # for the excel sheet control
-    # only the main program use this method to separate the excel and program
-    # since here will include the loop structure and overall control of report generation
-    import sheet_ctrl_main_obj as sh
-    import parameter_load_obj as para
-
-    # define the excel object
-    excel_m = para.excel_parameter(str(sh.file_setting))
-
-    pwr_m = inst.LPS_505N(
-    excel_m.pwr_vset,
-    excel_m.pwr_iset,
-    excel_m.pwr_act_ch,
-    excel_m.pwr_supply_addr,
-    excel_m.pwr_ini_state,)
-
-    pwr_m.sim_inst = 0
 
     # original version of definition => without instrument object input
     # NAGui = NAGuiRPC()
 
-    NAGui = NAGuiRPC(pwr0=pwr_m)
+    NAGui = NAGuiRPC(pwr0=pwr_m, excel0=excel_m)
 
     # # 1-line function - method 1
     # result = NAGui.call('GI2C.read(0x9E, 0x00, 1)')
@@ -401,9 +537,17 @@ Efficiency.Run()
         target_sheet = ''
 
 
-        NAGui.buck_regulation_mix(mode0=0, setting_sel0='virtual')
+        NAGui.buck_regulation_mix(mode0=0, setting_sel0='virtual', L_H0=1)
+
+
 
         pass
+
+    elif test_index == 2.8 :
+        '''
+        mutiple run and check average value
+        '''
+
 
     elif test_index == 3 :
         '''
